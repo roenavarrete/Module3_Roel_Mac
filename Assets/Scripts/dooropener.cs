10aebfffbf902989a3c2ce80653607728bf9ae0a
Roel Navarrete <rnavarrete2@huskers.unl.edu>
2025-02-14 17:03:37 -0600

    initial commit

--- Assets/Scripts/dooropener.cs
+++ Assets/Scripts/dooropener.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class dooropener : MonoBehaviour
 {
 
-
+    public GameObject door;
+    private batterydetector batterydetector;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        batterydetector = door.GetComponent<batterydetector>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (batterydetector.batterynumber >= 3)
+        {
+            openDoor();
+        }
+    }
+
+    void openDoor()
+    {
+
     }
 }
