da3035d8f676ce9f41c238975387b3bf4b66334b
Roel Navarrete <rnavarrete2@huskers.unl.edu>
2025-02-19 17:26:59 -0600

    Added alien idle route
    + Idle route tracing rooms and hallways

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,19 +6,48 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
+    public List<Transform> waypoints;
+
+    private int currentIndex = 1;
+    private Transform currentTarget;
+    private bool gotToCheckpoint = false;
+
     private NavMeshAgent navMeshAgent;
+    private bool playerInSight = false;
+
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        currentTarget = waypoints[currentIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player != null)
+        if (player != null && playerInSight)
         {
             navMeshAgent.SetDestination(player.position);
+        } else if (Vector3.Distance(transform.position, currentTarget.position) <= 1f && !gotToCheckpoint)
+        {
+            gotToCheckpoint = true;
+            MoveToNextWaypoint();
+        } else {
+            navMeshAgent.SetDestination(currentTarget.position);
+            gotToCheckpoint = false;
         }
+    }
+
+    void MoveToNextWaypoint()
+    {
+        if (currentIndex == waypoints.Count)
+        {
+            currentIndex = 0;
+        } else
+        {
+            currentIndex++;
+        }
+        currentTarget = waypoints[currentIndex];
+        navMeshAgent.SetDestination(currentTarget.position);
     }
 }
