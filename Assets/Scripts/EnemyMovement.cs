7b3cbffee18f8ee37d7b8b783508f5715b6b6f73
Roel Navarrete <rnavarrete2@huskers.unl.edu>
2025-02-21 03:18:40 -0600

    Added Field of View
    + Field of view cone for enemy
    Enemy will chase the player when the player is in view. If the player escapes, the enemy will go towards nearest waypoint.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -13,13 +13,14 @@ public class EnemyMovement : MonoBehaviour
     private bool gotToCheckpoint = false;
 
     private NavMeshAgent navMeshAgent;
-    private bool playerInSight = false;
+    private FieldOfView fieldOfView;
     private bool beingChased = false;
 
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        fieldOfView = GetComponent<FieldOfView>();
         currentTarget = waypoints[currentIndex];
         navMeshAgent.SetDestination(currentTarget.position);
     }
@@ -27,11 +28,11 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player != null && playerInSight) //Player is within alien vision
+        if (player != null && fieldOfView.canSeePlayer) //Player is within alien vision
         {
             navMeshAgent.SetDestination(player.position);
             beingChased = true;
-        } else if (!playerInSight && beingChased) //Player got away
+        } else if (!fieldOfView.canSeePlayer && beingChased) //Player got away
         {
             MoveToClosestWaypoint();
         } else if (Vector3.Distance(transform.position, currentTarget.position) <= 1f && !gotToCheckpoint)
@@ -69,7 +70,7 @@ public class EnemyMovement : MonoBehaviour
 
     void MoveToNextWaypoint()
     {
-        if (currentIndex < waypoints.Count)
+        if (currentIndex < waypoints.Count - 1)
         {
             currentIndex++;
         } else
